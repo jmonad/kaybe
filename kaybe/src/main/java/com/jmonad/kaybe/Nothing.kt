305aca95a24bf5dff6b23c2a813d47b5b09824f8
Pedro Paulo Amorim <pedro.amorim@vejoaovivo.com.br>
2016-04-15 14:40:00 -0300

    sample added, under development

--- kaybe/src/main/java/com/jmonad/kaybe/Nothing.kt
+++ kaybe/src/main/java/com/jmonad/kaybe/Nothing.kt
@@ -3,7 +3,9 @@ package com.jmonad.kaybe
 import com.jmonad.kaybe.lambda.Function
 
 class Nothing<A> : IMaybe<A> {
-  override fun <B> bind(fn: Function<B, A>): IMaybe = this
+  override fun <B> bind(fn: Function<B, A>): IMaybe<B> {
+    throw UnsupportedOperationException()
+  }
   override fun fromJust(): A = throw RuntimeException("Cannot call fromJust() on Nothing")
   override fun fromMaybe(def: A) : A = def
   override fun isJust() = false
